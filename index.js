c81739c7c707b913ea209b00f10c8503e8063e56
marcus featherston <marcusovfeatherston@gmail.com>
2023-01-14 18:09:46 -0700

    completed flow lab

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distanceTravelled) {
+  if (distanceTravelled <= 400) {
+    return "This one is on me!";
+  } else if (distanceTravelled > 400 && distanceTravelled < 2000) {
+    return "That will be twenty bucks.";
+  } else if (distanceTravelled > 2000 && distanceTravelled <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (distanceTravelled > 2500) {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(location) {
+  return location === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
